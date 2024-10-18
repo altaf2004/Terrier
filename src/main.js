7a307bf19b97ee084bf7b97fe3fb0e927e90a6d4
altaf2004 <altafhashmi2004@gmail.com>
2024-10-18 09:41:07 +0000

    final done

--- src/main.js
+++ src/main.js
@@ -1,61 +1,67 @@
-console.log("connected");
-
-const selecteEl = document.querySelector("select");
+// DOM Selection
+const selectEl = document.querySelector("select");
 const carouselContainerEl = document.querySelector(".carousel-inner");
-
-console.log(selecteEl, carouselContainerEl);
 
 const BASE_URL = "https://dog.ceo/api/";
 
-// ===Mark fetch
-//Gets the lits of api
+// === MARK: Fetch
+// Gets the list of dog breeds from api
 async function getDogsList() {
   try {
     const response = await fetch(`${BASE_URL}breeds/list/all`);
     const data = await response.json();
     return Object.keys(data.message);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 }
-getDogsList();
 
-//Get list of 10 images on breed
-async function getDogsImages(breed) {
+// Gets list of 10 images on breed
+async function getDogImages(breed) {
   try {
     const res = await fetch(`${BASE_URL}breed/${breed}/images`);
-    const data = await response.json();
+    const data = await res.json();
     return data.message.slice(0, 10);
-    console.log(data);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 }
 
-console.log(getDogsList);
-
-//=== Mark: Render
-async function RenderOption() {
+// === MARK: Render
+async function renderOptions() {
   const breedList = await getDogsList();
 
   for (breed of breedList) {
-    // ek option ke under breed ka naam
     const option = document.createElement("option");
-
     option.textContent = breed[0].toUpperCase() + breed.slice(1).toLowerCase();
     option.value = breed;
-
-    selecteEl.appendChild(option);
+    selectEl.appendChild(option);
   }
 }
 
-RenderOption();
+renderOptions();
 
-async function RenderCarousel(breed) {
-  const images = await getDogsImages(breed);
-  console.log(images);
+async function renderCarousel(breed) {
+  carouselContainerEl.innerHTML = "";
+  const images = await getDogImages(breed);
+
+  //   Add active to just the first item
+  for (let i = 0; i < images.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("carousel-item", i === 0 && "active");
+    div.innerHTML = `<img
+              src="${images[i]}"
+              class="d-block w-100 rounded-3"
+              alt="..."
+            />`;
+    carouselContainerEl.appendChild(div);
+  }
 }
 
-selecteEl.addEventListener("change", function (e) {
-  console.log(e.target.value);
+// This runs on select change
+selectEl.addEventListener("change", function (e) {
+  renderCarousel(e.target.value);
 });
+
+renderCarousel("affenpinscher");
+
